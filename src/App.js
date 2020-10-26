f09a803154cdcd639e3fe984966f6fd45ee6e35e
Artem Geroyan <geroyan.artem@mail.ru>
2020-10-26 22:18:20 +0300

    HomeWork 1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import cn from 'classnames';
+
+import './index.scss';
+import s from './App.modules.scss';
+
+const App = () => <div className={cn(s.color, 'header')}>App</div>;
+
+export default App;
